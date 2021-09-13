7272ea59d52a3342f675fcd0d50d1cc8ae47a15f
Oscar Acevedo <oscar.jair.it@gmail.com>
2021-09-13 10:50:11 -0300

    Update app

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+//Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+app.use(cors());
+
+// Body parser
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
+
+app.get("/", (req, res) => res.send("API Running"));
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
-});
+module.exports = app;
